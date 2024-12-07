6686f799e7c3b9dacec947d8c6f11059406b519c
nitinpatil2468 <nitin@getpi.in>
2024-12-07 20:02:04 +0530

    Initial Commit

---
+++ Rishta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rishta
+//
+//  Created by Abcom on 07/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rishta/RishtaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RishtaApp.swift
+//  Rishta
+//
+//  Created by Abcom on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RishtaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
